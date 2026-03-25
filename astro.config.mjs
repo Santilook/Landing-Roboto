19736836418b546363495f4bf2d1bfda48588e33
Santilook <santinahuel200@gmail.com>
2026-03-24 23:02:39 -0300

    feat: Se añadio el SEO de la pagina

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,18 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 
-
+import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://landing-roboto.vercel.app",
   output: "server",
 
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   adapter: vercel()
 });
